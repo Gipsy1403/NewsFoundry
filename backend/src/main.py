c092fe56b4efd0569d65904f019f2e6b4c9cd9a1
rouaret.gipsy@orange.fr <Gipsy1403>
2026-06-04 18:02:34 +0200

    fix cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,6 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "https://newsfoundry.vercel.app"
     ],
-    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
