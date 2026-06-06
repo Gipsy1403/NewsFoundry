f6bad7f9ac8f5ac967e15f04fd656353f79d2cc0
rouaret.gipsy@orange.fr <Gipsy1403>
2026-06-06 18:29:33 +0200

    ajout des tests pytest

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -35,23 +35,23 @@ def login(data: LoginRequest, session: Session = Depends(get_session)):
 #     with Session(engine) as session:
 
         # 1. Chercher utilisateur en base
-        user = session.exec(
+    user = session.exec(
             select(User).where(User.email == data.email)
         ).first()
 
         # 2. Vérification unique (sécurité)
-        if not user or not pwd_context.verify(data.password, user.hashed_password):
+    if not user or not pwd_context.verify(data.password, user.hashed_password):
             raise HTTPException(
                 status_code=401,
                 detail="Identifiants invalides"
             )
 
         # 4. Créer token JWT
-        token = create_access_token(
+    token = create_access_token(
             data={"user_id": user.id, "email": user.email}
         )
 
-        return {
+    return {
             "access_token": token,
             "token_type": "bearer"
         }
