22eb17ad1032b5df7e340aaa0765dc6ba654b0be
rouaret.gipsy@orange.fr <Gipsy1403>
2026-06-25 09:37:06 +0200

    basculement production

--- backend/src/main.py
+++ backend/src/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
      #    "https://newsfoundry.vercel.app",
-        "https://news-foundry-frontend.vercel.app"
+        "https://news-foundry-delta.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
